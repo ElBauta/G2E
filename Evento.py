ddae14f06edb23796d268dfb031363806e7db09c
Ernesto Gómez <ernesto0lag@gmail.com>
2026-07-23 16:41:36 -0400

    agregados los metodos to_dict y from_dict a la clase Event

--- Evento.py
+++ Evento.py
@@ -1,4 +1,5 @@
 # buscar dataclasses
+from datetime import datetime, date
 
 class Evento:
 
@@ -23,3 +24,17 @@ class Evento:
         texto += f"Categoria: {self.categoria}"
 
         return texto
+
+    def to_dict(self):
+        return{
+            "nombre" : self.nombre,
+            "fecha" : self.fecha.strftime("%d/%m/%Y"),
+            "hora" : self.hora,
+            "ubicacion" : self.ubicacion,
+            "descripcion" : self.descripcion,
+            "categoria" : self.categoria
+        }
+    @staticmethod
+    def from_dict(d):
+        fecha = datetime.strptime(d["fecha"], "%d/%m/%Y").date()
+        return Evento(d["nombre"], fecha, d["hora"], d["ubicacion"], d["categoria"], d["descripcion"])
