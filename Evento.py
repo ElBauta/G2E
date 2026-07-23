39233a9ba51a996f538996694333d388d63d9c40
Ernesto Gómez <ernesto0lag@gmail.com>
2026-07-23 03:04:59 -0400

    mejora de __str__ para evitar descripcion y inicio de gestor(incompleto)

--- Evento.py
+++ Evento.py
@@ -1,6 +1,6 @@
 class Evento:
 
-    def __init__(self, nombre, fecha, hora, ubicacion, descripcion, categoria):
+    def __init__(self, nombre, fecha, hora, ubicacion, categoria, descripcion = ""):
 
         self.nombre = nombre 
         self.fecha = fecha
@@ -10,10 +10,14 @@ class Evento:
         self.categoria = categoria
 
     def __str__(self):
+        texto = (f"Evento: {self.nombre}\n"
+                 f"Fecha: {self.fecha}\n"
+                 f"Hora: {self.hora}\n"
+                 f"Ubicacion: {self.ubicacion}\n")
 
-        return (f"Evento: {self.nombre}\n"
-                f"Fecha: {self.fecha}\n"
-                f"Hora: {self.hora}\n"
-                f"Ubicacion: {self.ubicacion}\n"
-                f"Descripcion: {self.descripcion}\n"
-                f"Categoria: {self.categoria}")
+        if self.descripcion != "":
+            texto += f"Descripcion: {self.descripcion}\n"
+
+        texto += f"Categoria: {self.categoria}"
+
+        return texto
